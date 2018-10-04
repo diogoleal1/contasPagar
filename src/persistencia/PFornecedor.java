77de8c7a07692cc1301dd41d164cde00c4cba2bd
diogoleal1 <dt1_diogo@hotmail.com>
2018-10-03 21:05:13 -0300

    .

--- src/persistencia/PFornecedor.java
+++ src/persistencia/PFornecedor.java
@@ -23,9 +23,9 @@ public class PFornecedor {
     public void incluir(Fornecedor parametro) throws SQLException {
 
         //Cria a instrução sql para a inserção de registros
-        String sql = "INSERT INTO"
-                + " fornecedor (nome, cpf, endereco, telefone, email, ie, ativo) "
-                + " VALUES (?,?,?,?,?,?,0)";
+        String sql = " INSERT INTO "
+                   + " pessoa (nome, cpf, endereco, telefone, email, ie, ativo) "
+                   + " VALUES (?,?,?,?,?,?,0) ";
 
         //Cria a conexao a partir dos métodos da fábrica de conexões
         Connection cnn = util.Conexao.getConexao();
@@ -34,11 +34,35 @@ public class PFornecedor {
         PreparedStatement prd = cnn.prepareStatement(sql);
 
         //Trocando os valores da ? por valores recebidos no método
-        prd.setString(1, parametro.getDescricao());
-        prd.setDouble(2, parametro.getValorMensalidade());
+        prd.setString(1, parametro.getNome());
+        prd.setString(2, parametro.getCpf_Cnpj());
+        prd.setString(3, parametro.getEndereco());
+        prd.setString(4, parametro.getTelefone());
+        prd.setString(5, parametro.getEmail());
+        prd.setString(6, parametro.getIe());
         
         prd.execute();
         cnn.close();
+    }
+    
+    public void alterar(Fornecedor parametro) throws SQLException {
+        
+        String sql = " UPDATE pessoa "
+                   + " SET nome = ?, cpf = ?, endereco = ?, id_tipoassociado = ? "
+                   + " WHERE identificador = ?;";
+
+        Connection cnn = util.Conexao.getConexao();
+        PreparedStatement ps = cnn.prepareStatement(sql);
+
+        ps.setString(1, parametro.getNome());
+        ps.setString(2, parametro.getEndereco());
+        ps.setString(3, parametro.getTelefone());
+        ps.setInt(4, parametro.getTipoAssociado().getIdentificador());
+        ps.setInt(5, parametro.getIdentificador());
+
+        ps.execute();
+        cnn.close();
+
     }
 
 }
